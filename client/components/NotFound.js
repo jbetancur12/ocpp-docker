2fd9b970654e129ed2cb8de01561e1b02f1a9744
Jorge Betancur <jbetancur@elespectador.com>
2021-11-25 21:12:49 +0000

    prettier

--- client/components/NotFound.js
+++ client/components/NotFound.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const NotFoundPage = () => {
-  return (<div>Los sentimos <h1>404 Page NotFound</h1></div>);
+  return (
+    <div>
+      Los sentimos <h1>404 Page NotFound</h1>
+    </div>
+  );
 };
 
 export default NotFoundPage;
