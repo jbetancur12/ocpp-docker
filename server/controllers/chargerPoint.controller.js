4aaa617febe9bc56fa0cb80a00efc2fb11ce9e9a
Jorge Betancur <jabetancur12@gmail.com>
2023-03-17 22:10:51 +0000

    add

--- server/controllers/chargerPoint.controller.js
+++ server/controllers/chargerPoint.controller.js
@@ -214,7 +214,7 @@ const stop = async (req, res) => {
         console.log("🚀 ~ file: chargerPoint.controller.js:187 ~ stop ~ client", client)
         const CP = await ChargerPoint.findOne({ charger_box_id: client })
         console.log("🚀 ~ file: chargerPoint.controller.js:216 ~ stop ~ CP:", CP)
-        const transactionId = await Transaction.find({ user: CP._id }).sort({ createdAt: -1 }).limit(1)
+        const transactionId = await Transaction.find({ chargerPointId: CP._id }).sort({ createdAt: -1 }).limit(1)
         console.log("🚀 ~ file: chargerPoint.controller.js:188 ~ stop ~ transactionId", transactionId)
 
         const ms = await centralSystem.stop(
